0bc3326dfaad93d88e98c92eb33eb4197fe11b7c
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-05 12:33:17 -0700

    Add test for instance of Turn, players, and default spoils

---
+++ test/turn_test.rb
@@ -0,0 +1,42 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+
+    @cards1 = [@card1, @card2, @card5, @card8]
+    @cards2 = [@card3, @card4, @card6, @card7]
+
+    @deck1 = Deck.new(@cards1)
+    @deck2 = Deck.new(@cards2)
+
+    @player1 = Player.new('Megan', @deck1)
+    @player2 = Player.new('Aurora', @deck2)
+
+    @turn = Turn.new(@player1, @player2)
+  end
+
+  def test_it_exist
+    assert_instance_of Turn, @turn
+  end
+
+  def test_turn_has_players
+    assert_equal 'Megan', @turn.player1.name
+    assert_equal 'Aurora', @turn.player2.name
+  end
+
+  def test_spoils_of_war_is_empty_by_default
+    assert_equal [], @turn.spoils_of_war
+  end
+
+end
