f39b5df2736669cf87ad682737efa2d110937800
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-05 13:00:21 -0700

    Add type method and conditionals

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,5 @@
+require './lib/player'
+
 class Turn
   attr_reader :player1,
               :player2,
@@ -8,4 +10,16 @@ class Turn
     @player2 = player2
     @spoils_of_war = []
   end
+
+  def type
+    if @player1.deck.cards[0].rank != @player2.deck.cards[0].rank
+      :basic
+    elsif @player1.deck.cards[2].rank == @player2.deck.cards[2].rank
+      :mutually_assured_destruction
+    else
+      :war
+    end  
+  end
+
+
 end
