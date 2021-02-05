40fb5159930e2f9af05c0630036b9d4bf97fb3c7
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-05 08:39:52 -0700

    Add high_ranking_cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -18,4 +18,13 @@ class Deck
     card_ranking
   end
 
+  def high_ranking_cards
+    high_cards = []
+    cards.each do |card|
+      if card.rank >= 11
+        high_cards << card
+      end
+    end
+    high_cards
+  end
 end
