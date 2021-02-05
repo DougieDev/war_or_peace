3121d401cb9075806eb1eb9139afe1a430d7e709
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-05 08:24:03 -0700

    Add rank_of_card_at method

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,5 +6,16 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
-  
+
+  def rank_of_card_at(index)
+    if index == 0
+      card_ranking = cards[0].rank
+    elsif index == 2
+      card_ranking = cards[2].rank
+    else
+      card_ranking = nil
+    end
+    card_ranking
+  end
+
 end
