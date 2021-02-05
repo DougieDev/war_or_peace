c12f9820443f84b01788c5838d806763eb581a1c
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-05 08:04:15 -0700

    Add test for card attributes

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,14 +5,20 @@ require './lib/card'
 
 class DeckTest < Minitest::Test
   def setup
-    @card1 = Card.new('diamond', 'Queen', 12)
-    @card2 = Card.new('spade', '3', 3)
-    @card3 = Card.new('heart', 'Ace', 14)
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
     @cards = [@card1, @card2, @card3]
     @deck = Deck.new(@cards)
   end
 
   def test_it_exists
     assert_instance_of Deck, @deck
+  end
+
+  def test_card_attributes
+    assert_equal :diamond, @deck.cards[0].suit
+    assert_equal 3, @deck.cards[1].rank
+    assert_equal 'Ace', @deck.cards[2].value
   end
 end
